3fe9fc6db3df7a92c84c8ee280eceaaeed958fac
mihermanogordo <andreo.olivella@gmail.com>
2026-04-29 13:58:24 +0200

    plan

--- front/src/app/app.ts
+++ front/src/app/app.ts
@@ -16,5 +16,5 @@ import { CommonModule } from '@angular/common';
 export class App {
   protected readonly title = signal('front');
 
-  constructor(public authService: AuthService) {}
+  constructor(public authService: AuthService) { }
 }
